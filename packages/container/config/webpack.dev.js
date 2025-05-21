2b2f24f68b1e7679f3f82f71377a6b2588fa6eba
Manuel Garcia <manolete919@gmail.com>
2025-05-21 16:02:12 +0000

    authentication, corrections

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,28 +1,29 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: "development",
+  mode: 'development',
   output: {
     publicPath: 'http://localhost:9090/',
   },
   devServer: {
     port: 9090,
     historyApiFallback: {
-      //index: '/index.html',
       historyApiFallback: true,
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:9091/remoteEntry.js',
-      }
-    })
-  ]
+        auth: 'auth@http://localhost:9092/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
